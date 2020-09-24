27a32190bee9c47ca3806a74e5e4cc8f94f6905c
Anna Startseva <a.v.starceva@gmail.com>
2020-09-25 02:03:16 +0600

    feat: create base structure of code

---
+++ calculator/script.js
@@ -0,0 +1,31 @@
+var numbers = document.querySelectorAll('.number'),
+    operations = document.querySelectorAll('.operator'),
+    decimal = document.getElementById('decimal'),
+    ce = document.getElementById('ce'),
+    c = document.getElementById('c');
+result = document.getElementById('result');
+
+console.log(decimal);
+console.log(ce);
+console.log(c);
+console.log(result);
+
+function numberPress() {
+
+};
+
+function operation() {
+
+};
+
+function decimal() {
+
+};
+
+function clear() {
+
+};
+
+function howWork() {
+
+};
