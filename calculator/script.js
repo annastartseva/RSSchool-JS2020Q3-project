4f72eb95bcc7fa0877fcd46471bf7054b372c12f
Anna Startseva <a.v.starceva@gmail.com>
2020-09-26 02:47:36 +0600

    feat: add a button click function

--- calculator/script.js
+++ calculator/script.js
@@ -1,24 +1,29 @@
 var numberBtns = document.querySelectorAll('.number'),
     operationBtns = document.querySelectorAll('.operator'),
+    display = document.getElementById('display'),
     decimalBtn = document.getElementById('decimal'),
     clearBtns = document.querySelectorAll('.clear-btn'),
-    resultBtn = document.getElementById('result');
+    resultBtn = document.getElementById('result'),
+    memoryCurrentNumber = 0,
+    memoryNewNumber = false,
+    memoryPendingOperation = '';
 
-
+console.log(decimalBtn);
+console.log(decimalBtn);
+console.log(resultBtn);
 for (var i = 0; i < numberBtns.length; i++) {
     var number = numberBtns[i];
     number.addEventListener('click', function(e) {
         numberPress(e.target.textContent);
     });
-};
+}
 
 for (var i = 0; i < operationBtns.length; i++) {
     var operationBtn = operationBtns[i];
     operationBtn.addEventListener('click', function(e) {
         operation(e.target.textContent);
     });
-
-};
+}
 
 for (var i = 0; i < clearBtns.length; i++) {
     var clearBtn = clearBtns[i];
@@ -26,14 +31,19 @@ for (var i = 0; i < clearBtns.length; i++) {
         //console.log(e.srcElement.id);
         clear(e.srcElement.id);
     });
-
-};
+}
 
 decimalBtn.addEventListener('click', decimal);
 resultBtn.addEventListener('click', result);
 
+
 function numberPress(symbolNum) {
     console.log('Клик по кнопке с номером ' + symbolNum);
+    if (display.value === '0') {
+        display.value = symbolNum;
+    } else {
+        display.value += symbolNum;
+    }
 };
 
 function operation(symbol) {
